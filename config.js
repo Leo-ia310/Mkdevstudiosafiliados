9d119be9ccb5764c72d9f1c39a7c345df7fa22a5
Maikel <153005240+Leo-ia310@users.noreply.github.com>
2026-02-24 22:34:49 -0600

    Agrgando researcher

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const CONFIG = {
-    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyMNF5SjekgnbULIF59XnHafYDpKO4xhO-jPHR64HHidX6TNmqEsF0Ox6NG2_UvsAaX_Q/exec'
+    APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzYkr5oTuKJgpblu9V1ALpMKglUynH_yUZm7dUHsxjvzcQkJhYbR9EXgklY0x-UeY5L4Q/exec'
 };
